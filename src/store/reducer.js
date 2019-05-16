d1834e6f9952dc0b07a3d7474f4131a9ea532cb7
Khoi Nguyen <ntkhoiptit@gmail.com>
2019-05-17 01:05:08 +0700

    242 Passing and Retrieving Data with Action.mp4

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,30 @@
 const initialState = {
-  counter: 1
+    counter: 1
 }
 
-const reducer = (state = initialState , action) => {
-  if (action.type === 'INCREMENT' ) {
-    return {
-      counter: state.counter + 1
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
     }
-  }
-  return state;
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if( action.type === 'ADD') {
+        return {
+            counter: state.counter + action.val
+        }
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.val
+        }
+    }
+    return state;
 }
 
 export default reducer;
